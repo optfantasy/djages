87a422cf10001166705d3588bd042c718b0e6ad8
Gage Tseng <gagedark@gmail.com>
2013-11-14 11:37:49 +0800

    fix deploy scripts

--- fabfile.py
+++ fabfile.py
@@ -72,6 +72,7 @@ def new_user(username, public_key_path):
             username=username))
         run('sudo adduser {username} sudo'.format(
             username=username))
+        run('echo "%s ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers' % username)
 
         f = file(public_key_path)
         key = f.read()
@@ -87,12 +88,28 @@ def new_user(username, public_key_path):
 
 
 def install_packages():
-    command = 'sudo apt-get update;sudo apt-get install python-software-properties python g++ make git python-pip python-virtualenv python-imaging build-essential python-dev libxml2-dev libxslt-dev python-lxml libssl-dev libpam0g-dev nginx apache2'
+    command = 'sudo apt-get update;sudo apt-get -y install python-software-properties python g++ make git python-pip python-virtualenv python-imaging build-essential python-dev libxml2-dev libxslt-dev python-lxml libssl-dev libpam0g-dev nginx apache2 libapache2-mod-wsgi'
     run(command)
+    if exists('/etc/nginx/sites-enabled/default'):
+        run('sudo rm -r /etc/nginx/sites-enabled/default')
+    if exists('/etc/apache2/sites-enabled/000-default.conf'):
+        run('sudo rm -r /etc/apache2/sites-enabled/000-default.conf')
+    if exists('/etc/apache2/sites-enabled/000-default'):
+        run('sudo rm -r /etc/apache2/sites-enabled/000-default')
+
+    run('sudo a2enmod headers')
+    run('sudo a2enmod expires')
+
+
+def install_mongodb():
+    command = 'wget https://raw.github.com/gage/server_scripts/master/mongodb_install.sh -v -O mongodb_install.sh;chmod +x ./mongodb_install.sh;./mongodb_install.sh; rm -rf mongodb_install.sh'
+    with prefix('cd /tmp'):
+        run(command)
 
 def create_env():
     if not exists('%(path)s' % env):
         run('sudo mkdir -p %(path)s' % env)
+        run('sudo mkdir -p /home/%(project_name)s/logs' % env)
         run('sudo chown -R %(project_name)s:%(project_name)s /home/%(project_name)s/site' % env)
 
 def create_repo():
@@ -100,6 +117,12 @@ def create_repo():
     if not exists('%(repo_path)s' % env):
         with prefix('cd %(path)s' % env):
             run('git clone %s repository' % env.repo_url)
+    if not exists('%(repo_path)s/%(project_name)s/configs/common/jinja_cache' % env):
+        run('mkdir -p %(repo_path)s/%(project_name)s/configs/common/jinja_cache' % env)
+    run('sudo chown -R www-data:www-data %(repo_path)s/%(project_name)s/configs/common/jinja_cache' % env)
+    if not exists('%(repo_path)s/%(project_name)s/configs/staging/jinja_cache' % env):
+        run('mkdir -p %(repo_path)s/%(project_name)s/configs/staging/jinja_cache' % env)
+    run('sudo chown -R www-data:www-data %(repo_path)s/%(project_name)s/configs/staging/jinja_cache' % env)
 
 def setup_vitrualenv():
     env.user = env.project_name
@@ -112,6 +135,7 @@ def setup_vitrualenv():
 
 def ubuntu_init(public_key_path):
     install_packages()
+    install_mongodb()
     new_user(env.project_name, public_key_path)
     create_env()
     create_repo()
