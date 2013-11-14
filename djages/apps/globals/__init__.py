ce4832ba26bab0baa8f512ec79c34c791f8735e2
Gage Tseng <gagedark@gmail.com>
2013-11-14 12:04:31 +0800

    add create_anonymous_user

--- djages/apps/globals/__init__.py
+++ djages/apps/globals/__init__.py
@@ -1,6 +1,19 @@
 from djangotoolbox.fields import AbstractIterableField
+from django.contrib.auth.models import User
 
 def dummy(self, **kwargs):
 	pass
 
 AbstractIterableField.formfield = dummy
+
+def create_anonymous_user(**kwargs):
+    """ Creates anonymous User instance. """
+    try:
+        User.objects.get(username="AnonymousUser")
+    except User.DoesNotExist:
+        User.objects.create(username='AnonymousUser')
+    except User.MultipleObjectsReturned:
+        for u in User.objects.filter(username="AnonymousUser")[1:]:
+            u.delete()
+
+create_anonymous_user()
